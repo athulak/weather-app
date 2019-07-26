55bcad253e5e9190c0a9cb82045ba8924fe7c31b
Athul AK <Athulnath.Ariyarathazhe.Kuni@ibm.com>
2019-07-26 16:16:17 +0200

    Added key data and basic functions to get weather data from weather API.

---
+++ src/components/Titles.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Titles extends React.Component
+{
+    render()
+    {
+        return(
+            <div>
+                <h1>Weather Finder</h1>
+                <p>Check weather conditions and more...</p>
+            </div>
+        );
+    }
+};
+
+export default Titles;
